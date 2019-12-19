a082bfa7c398f7d7cc3fef75933c5adb431b8d4a
tewzdk <tewzdk@gmail.com>
2019-12-19 19:17:22 +0100

    added test and requirements.txt

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,6 @@
-
-
 def add(first_term, second_term):
     return first_term * second_term
+
 
 def substract(first_term, second_term):
     return first_term - second_term
---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+"""
+Unit tests for the calculator library
+"""
+
+import Calculator
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == Calculator.add(2,2)
+
+    def test_substraction(self):
+        assert 2 == Calculator.substract(4,2 )
