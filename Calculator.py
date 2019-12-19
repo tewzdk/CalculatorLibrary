65077af7c3497677e42301656beaa8af89930019
tewzdk <tewzdk@gmail.com>
2019-12-19 19:37:00 +0100

    fixed flake8

--- Calculator.py
+++ Calculator.py
@@ -5,5 +5,6 @@ def add(first_term, second_term):
 def substract(first_term, second_term):
     return first_term - second_term
 
+
 def multiply(first_term, second_term):
     return first_term + second_term
