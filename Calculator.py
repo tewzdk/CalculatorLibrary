47be083c4e8bff5a79c5eb4df8f6bab9c30d42f8
tewzdk <tewzdk@gmail.com>
2019-12-19 19:35:32 +0100

    added multiply

--- Calculator.py
+++ Calculator.py
@@ -4,3 +4,6 @@ def add(first_term, second_term):
 
 def substract(first_term, second_term):
     return first_term - second_term
+
+def multiply(first_term, second_term):
+    return first_term + second_term
