ad56a8fba64eb491ef5461624a94066d69c1ebca
tewzdk <tewzdk@gmail.com>
2019-12-19 19:32:25 +0100

    fixed syntax

--- test_calculator.py
+++ test_calculator.py
@@ -4,10 +4,11 @@ Unit tests for the calculator library
 
 import Calculator
 
+
 class TestCalculator:
 
     def test_addition(self):
-        assert 4 == Calculator.add(2,2)
+        assert 4 == Calculator.add(2, 2)
 
     def test_substraction(self):
-        assert 2 == Calculator.substract(4,2 )
+        assert 2 == Calculator.substract(4, 2)
